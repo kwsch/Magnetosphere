a52100ced19e208b9e2c26a2881e9e3c4467bc7f
Kurt <kaphotics@gmail.com>
2019-03-10 13:15:44 -0700

    Add citra timeout checks
    I'm able to crash Citra if I request memory at a certain region (bug?);
    the user can also close the app so we should really throw an exception
    instead of infinite-looping.

--- Magnetosphere/Connections/Communicators/Citra/CitraTranslator.cs
+++ Magnetosphere/Connections/Communicators/Citra/CitraTranslator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 
 namespace Magnetosphere
@@ -10,11 +11,7 @@ namespace Magnetosphere
         public CitraTranslator(DeviceMessenger m) => Messenger = m;
 
         public byte[] Read(ulong offset, ulong length, long pid) => ReadMemory((uint)offset, (uint)length);
-
-        public void Write(byte[] data, ulong offset, long pid)
-        {
-            WriteMemory(data, (uint)offset);
-        }
+        public void Write(byte[] data, ulong offset, long pid) => WriteMemory(data, (uint)offset);
 
         private const uint CURRENT_REQUEST_VERSION = 1;
         private const uint MAX_REQUEST_DATA_SIZE = 32;
@@ -42,15 +39,14 @@ namespace Magnetosphere
         {
             var hdr = rawReply.ToClass<CitraPacketHeader>();
 
-            // should probably throw exceptions here for protocol breaks...
             if (CURRENT_REQUEST_VERSION != hdr.Version)
-                return Array.Empty<byte>();
+                throw new ArgumentException(nameof(CitraPacketHeader.Version));
             if (expectedID != hdr.PacketID)
-                return Array.Empty<byte>();
+                throw new ArgumentException(nameof(CitraPacketHeader.PacketID));
             if (expectedType != hdr.Type)
-                return Array.Empty<byte>();
+                throw new ArgumentException(nameof(CitraPacketHeader.Type));
             if (rawReply.Length - CitraPacketHeader.SIZE != hdr.Length)
-                return Array.Empty<byte>();
+                throw new ArgumentException(nameof(CitraPacketHeader.Length));
 
             if (hdr.Length == 0)
                 return Array.Empty<byte>();
@@ -59,11 +55,9 @@ namespace Magnetosphere
             return data;
         }
 
-        private readonly object transmission = new object();
-
         public byte[] ReadMemory(uint address, uint size)
         {
-            var result = new byte[(int)size];
+            var result = new byte[size];
 
             // request, read, repeat
             var ctr = size;
@@ -76,15 +70,7 @@ namespace Magnetosphere
                 BitConverter.GetBytes(address).CopyTo(readRPak, CitraPacketHeader.SIZE + 0);
                 BitConverter.GetBytes(readSize).CopyTo(readRPak, CitraPacketHeader.SIZE + 4);
 
-                byte[] response;
-                lock (transmission)
-                {
-                    Messenger.SendData(readRPak);
-                    while (!Messenger.Connection.HasPacketReady)
-                        Thread.Sleep(Messenger.Connection.ReadLoopInterval);
-                    Messenger.ReceiveData(out response);
-                }
-
+                var response = SendReceive(readRPak);
                 var data = ReadAndValidateHeader(response, readHdr.PacketID, readHdr.Type);
                 if (data.Length == 0)
                     continue;
@@ -112,20 +98,34 @@ namespace Magnetosphere
                 BitConverter.GetBytes(writesize).CopyTo(wData, CitraPacketHeader.SIZE + 4);
                 Array.Copy(data, ctr - writesize, wData, CitraPacketHeader.SIZE + 8, writesize);
 
-                byte[] response;
-                lock (transmission)
-                {
-                    Messenger.SendData(wData);
-                    while (!Messenger.Connection.HasPacketReady)
-                        Thread.Sleep(Messenger.Connection.ReadLoopInterval);
-                    Messenger.ReceiveData(out response);
-                }
-
+                var response = SendReceive(wData);
                 var rdata = ReadAndValidateHeader(response, hdr.PacketID, hdr.Type);
                 if (rdata.Length != 0)
                     continue;
                 ctr -= writesize;
                 address += writesize;
+            }
+        }
+
+        private readonly object transmission = new object();
+        private readonly Stopwatch TimeoutCheck = new Stopwatch();
+
+        private byte[] SendReceive(byte[] data)
+        {
+            lock (transmission)
+            {
+                Messenger.SendData(data);
+                TimeoutCheck.Restart();
+                while (!Messenger.Connection.HasPacketReady)
+                {
+                    if (TimeoutCheck.Elapsed >= TimeSpan.FromSeconds(2))
+                        throw new TimeoutException("Failed to receive a response from Citra.");
+                    Thread.Sleep(Messenger.Connection.ReadLoopInterval);
+                }
+
+                TimeoutCheck.Stop();
+                Messenger.ReceiveData(out var response);
+                return response;
             }
         }
     }
